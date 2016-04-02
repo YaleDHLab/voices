9ee2b4a87a34e31d3485b4f3324912c9f86291f9
duhaime <douglas.duhaime@gmail.com>
2016-04-02 11:49:01 -0400

    update environment config to support ruby-cas gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 VoicesRails::Application.initialize!
+
+# Support RubyCAS client for CAS authentication
+CASClient::Frameworks::Rails::Filter.configure(
+  :cas_base_url => "https://secure.its.yale.edu/cas/"
+)
