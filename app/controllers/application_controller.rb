945be0e71d17433e9d92c10b28088bd08531ac51
duhaime <douglas.duhaime@gmail.com>
2016-04-05 11:07:48 -0400

    add a method to the application_controller to ensure that the user has the rights to access the record they've requested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def check_privileges(requested_record)
+    redirect_to "/", notice: 'You dont have enough permissions to be here' unless requested_record.cas_user_name == session[:cas_user]
+  end
 end
