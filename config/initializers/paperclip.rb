c25d600821acd03db81ee9021098d41819bd2ca8
duhaime <douglas.duhaime@gmail.com>
2016-04-04 20:17:14 -0400

    add new logic to the paperclip initializer to set the path to the fallback image for missing images

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,8 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+# see SO thread 9646549 for a discussion of this method
+# config/initializers/paperclip.rb
+Paperclip.interpolates(:placeholder) do |attachment, style|
+  ActionController::Base.helpers.asset_path("missing_#{style}.png")
+end
