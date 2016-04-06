5a1b2e92e1dc561e571d3fb990c9ac39fa124154
duhaime <douglas.duhaime@gmail.com>
2016-04-06 15:54:30 -0400

    update records js to make the .gray-background div fill all vertical space from header to footer

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -1,8 +1,24 @@
 // helper to set the currently selected file in the form
 $(document).ready(function() {
 
-  // when user clicks the file upload button, 
-  // change the html of the placeholder box
+  /***
+  * ensure the gray-background div height fills the remaining space
+  ***/
+
+  var calculateBackgroundHeight = function() {
+    return $(document).height() - $("#header").height() - $("#footer").height() - $(".small-banner-image-container").height() - $(".record-details-top").height();
+  };
+
+  $(".gray-background").height( calculateBackgroundHeight() );
+
+  $(window).resize(function() {
+    $(".gray-background").height( calculateBackgroundHeight() );
+  });
+
+  /***
+  * when user clicks the file upload button, change the html of the placeholder box
+  ***/
+
   $("#custom-file-upload").on("change", function() {
     updatePlaceholderBox( $(this) );
   });
@@ -11,3 +27,4 @@ $(document).ready(function() {
     $("#placeholder-box").html( thisContext.val().split("/").pop().split("\\").pop() );
   };
 });
+
