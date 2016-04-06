a32dd6346f6d84e0950582e859ce467a365147cf
duhaime <douglas.duhaime@gmail.com>
2016-04-06 14:58:52 -0400

    many additions to record model to allow ingest of image, video, and audio and skip post-processing on audio content

--- app/models/record.rb
+++ app/models/record.rb
@@ -7,12 +7,61 @@ class Record < ActiveRecord::Base
   # paperclip processing: store a small, medium, and large copy of images
   # and store a thumb and medium size still of videos
   has_attached_file :file_upload,
-    styles: lambda {
-      |a| a.instance.is_image? ? {
+    styles: lambda { |a| a.instance.check_file_type },
+
+    processors: lambda { 
+      |a| a.is_video? ? [ :ffmpeg ] : [ :thumbnail ] 
+    }
+
+  # validate that we accept the type of file the user is uploading
+  validates_attachment_content_type :file_upload,
+    :content_type => [
+      "video/mp4", 
+      "image/jpg", 
+      "image/jpeg", 
+      "image/png", 
+      "image/gif",
+      "audio/mpeg", "audio/x-mpeg", "audio/mp3", "audio/x-mp3", "audio/mpeg3", "audio/x-mpeg3", "audio/mpg", "audio/x-mpg", "audio/x-mpegaudio"
+      ],
+    :message => "Please make sure you've attached a jpg, png, gif, or mp4 file"
+
+    before_post_process :skip_for_audio
+
+  # helper method that uses the =~ regex helper to see if 
+  # the current file_upload has a content_type 
+  # attribute that contains the string "image" / "video", or "audio"
+  def is_image?
+    file_upload.content_type =~ %r(image)
+  end
+
+  def is_video?
+    file_upload.content_type =~ %r(video)
+  end
+
+  def is_audio?
+    file_upload.content_type =~ /\Aaudio\/.*\Z/
+  end
+
+
+  # if the uploaded content type is found in the explicit array of audio types, 
+  # return false so that we'll skip audio post processing
+  def skip_for_audio
+    if is_audio? 
+      return false
+    else
+      return true
+    end
+  end
+
+  def check_file_type
+    if is_image?
+      {
         :small => "200x200>", 
         :medium => "300x300>", 
         :large => "600x600>"
-      } : {
+      }
+    elsif is_video?
+      {
         :thumb => { 
           :geometry => "100x100#", 
           :format => 'jpg', 
@@ -24,23 +73,15 @@ class Record < ActiveRecord::Base
           :time => 10
         }
       }
-    },
-
-    processors: lambda { 
-      |a| a.is_video? ? [ :ffmpeg ] : [ :thumbnail ] 
-    }
-
-  # validate that we accept the type of file the user is uploading
-  validates_attachment_content_type :file_upload,
-    :content_type => ["video/mp4", "image/jpg", "image/jpeg", "image/png", "image/gif"],
-    :message => "Please make sure you've attached a jpg, png, gif, or mp4 file"
-
-  def is_image?
-    file_upload.content_type =~ %r(image)
-  end
-
-  def is_video?
-    file_upload.content_type =~ %r(video)
+    elsif is_audio?
+      {
+        :audio => {
+          :format => "mp3"
+        }
+      }
+    else
+      {}
+    end
   end
     
 end
