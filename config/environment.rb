4704cc6314af415ffbe9d1ac16f784048cc95ffe
duhaime <douglas.duhaime@gmail.com>
2016-04-09 10:48:07 -0400

    update config/environment.rb to initialize the app after specifying cas auth details

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,11 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
-VoicesRails::Application.initialize!
-
 # Support RubyCAS client for CAS authentication
 CASClient::Frameworks::Rails::Filter.configure(
   :cas_base_url => "https://secure.its.yale.edu/cas/"
 )
+
+# Initialize the Rails application.
+VoicesRails::Application.initialize!
+
