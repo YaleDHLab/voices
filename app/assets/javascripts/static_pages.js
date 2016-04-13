d5fa9b628a85ee6b159d0d270fa58cac4d2bd6b9
duhaime <douglas.duhaime@gmail.com>
2016-04-13 12:33:19 -0400

    update static_pages.js to increase probability of making color lighter

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -41,7 +41,7 @@ $(document).on('ready', function() {
       else {
         var randomVariable = generateRandomNumber(0, 1);
         // specify the probability the word will increase in color
-        if (randomVariable > .01 ) {
+        if (randomVariable > .5 ) {
           currentTarget.style.color = colorRange[currentColorIndex + 1];
         }
         else {
