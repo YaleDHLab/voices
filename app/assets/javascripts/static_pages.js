7c108293530560ec17804b8fac682573234be3bd
duhaime <douglas.duhaime@gmail.com>
2016-04-16 10:48:31 -0400

    standardize spacing in static_pages.js

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -38,7 +38,7 @@ $(document).on('ready', function() {
 
     rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
     function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
+      return ("0" + parseInt(x).toString(16)).slice(-2);
     }
     return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
   };
