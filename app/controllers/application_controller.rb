540595681e4e56343cd43cca04c762f63c06b1bc
duhaime <douglas.duhaime@gmail.com>
2016-04-18 10:58:48 -0400

    add method to application controller to verify if a user has admin privileges for protected resources (namely records#index)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def check_privileges(requested_record)
+  def check_user_privileges(requested_record)
     if requested_record.cas_user_name != session[:cas_user]
+      flash[:info] = "<strong>ACCESS RESTRICTED</strong>".html_safe + ": You do not have access to this page. Please contact your administrator about your permissions."
+      redirect_to user_show_path
+    end
+  end
+
+  def check_admin_privileges()
+    site_admins = ENV["VOICES_ADMINS"].split("#")
+    if not site_admins.include? session[:cas_user]
       flash[:info] = "<strong>ACCESS RESTRICTED</strong>".html_safe + ": You do not have access to this page. Please contact your administrator about your permissions."
       redirect_to user_show_path
     end
