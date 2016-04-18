c7b300ff03bd708ab8bce48a0c587ef7f07cf434
duhaime <douglas.duhaime@gmail.com>
2016-04-18 17:07:37 -0400

    update db schema to include date in records fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407172018) do
+ActiveRecord::Schema.define(version: 20160418185803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160407172018) do
     t.string   "location"
     t.string   "source_url"
     t.boolean  "release_checked"
+    t.text     "date"
   end
 
   create_table "users", force: true do |t|
