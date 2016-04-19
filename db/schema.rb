c800aa37e2cffd1f39bf95be0ac025c1cbac0369
duhaime <douglas.duhaime@gmail.com>
2016-04-19 10:17:49 -0400

    udpate db schema to include field for file_upload_url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418185803) do
+ActiveRecord::Schema.define(version: 20160419014737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160418185803) do
     t.string   "source_url"
     t.boolean  "release_checked"
     t.text     "date"
+    t.text     "file_upload_url"
   end
 
   create_table "users", force: true do |t|
