fe4ae73f7c959c8524cc67439e950325563beabf
duhaime <douglas.duhaime@gmail.com>
2016-04-21 11:40:43 -0400

    update seeds to seed all admin profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-20.times do |r|
 
-  new_record = Record.new({
-    :title => Faker::Book.title,
-    :metadata => Faker::Lorem.sentence,
-    :file_upload => File.new( Dir.glob("#{Rails.root}/app/assets/images/seed-images/*.jpg")[rand(0..249)] ),
-    :cas_user_name => "mo273",
-    :include_name => true,
-    :content_type => "Image",
-    :description => Faker::Lorem.paragraph,
-    :location => Faker::Address.street_name + ", " + Faker::Address.city,
-    :source_url => Faker::Internet.url,
-    :release_checked => true,
-    :date => Faker::Time.between(DateTime.now - 1, DateTime.now).to_s.split()[0].gsub(/-/, '/'),
-    :hashtag => "#" + Faker::Hipster.words(4).join(" #")
-  })
+# seed each admin's database
+ENV["VOICES_ADMINS"].split("#").each do |username|
 
-  new_record.save!
+    20.times do |r|
+
+        new_record = Record.new({
+            :title => Faker::Book.title,
+            :metadata => Faker::Lorem.sentence,
+            :file_upload => File.new( Dir.glob("#{Rails.root}/app/assets/images/seed-images/*.jpg")[rand(0..249)] ),
+            :cas_user_name => username,
+            :include_name => true,
+            :content_type => "Image",
+            :description => Faker::Lorem.paragraph,
+            :location => Faker::Address.street_name + ", " + Faker::Address.city,
+            :source_url => Faker::Internet.url,
+            :release_checked => true,
+            :date => Faker::Time.between(DateTime.now - 1, DateTime.now).to_s.split()[0].gsub(/-/, '/'),
+            :hashtag => "#" + Faker::Hipster.words(4).join(" #")
+        })
+
+        new_record.save!
+    end
 end
