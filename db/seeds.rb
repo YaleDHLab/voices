f451b762eddd56764801699f8628c9b290236817
duhaime <douglas.duhaime@gmail.com>
2016-04-21 09:13:23 -0400

    create seeds.rb that uses faker and random images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do |r|
+
+  new_record = Record.new({
+    :title => Faker::Book.title,
+    :metadata => Faker::Lorem.sentence,
+    :file_upload => File.new( Dir.glob("#{Rails.root}/app/assets/images/seed-images/*.jpg")[rand(0..249)] ),
+    :cas_user_name => "ded34",
+    :include_name => true,
+    :content_type => "Image",
+    :description => Faker::Lorem.paragraph,
+    :location => Faker::Address.street_name + ", " + Faker::Address.city,
+    :source_url => Faker::Internet.url,
+    :release_checked => true,
+    :date => Faker::Time.between(DateTime.now - 1, DateTime.now).to_s.split()[0].gsub(/-/, '/'),
+    :hashtag => "#" + Faker::Hipster.words(4).join(" #")
+  })
+
+  new_record.save!
+end
