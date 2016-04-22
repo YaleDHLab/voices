b0b968c4a366470a37655465c06f36d5986bcbc7
duhaime <douglas.duhaime@gmail.com>
2016-04-22 09:35:31 -0400

    update paperclip initializer to manually specify mimetype of 3gpp files from android

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -7,3 +7,7 @@ Paperclip.interpolates(:placeholder) do |attachment, style|
   ActionController::Base.helpers.asset_path("missing_#{style}.png")
 end
 
+# add initializer that manually overrides 3gpp mimetype to audio
+android_audio_type = MIME::Types["audio/3gpp"].first
+android_audio_type.extensions << "3gpp"
+MIME::Types.index_extensions android_audio_type
