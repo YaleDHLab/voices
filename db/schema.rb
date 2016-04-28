a5c1d0f3721f414010ea6b530ddc5be444639bd2
duhaime <douglas.duhaime@gmail.com>
2016-04-28 15:35:14 -0400

    update schema given migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419171046) do
+ActiveRecord::Schema.define(version: 20160428191830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "contact_forms", force: true do |t|
+  create_table "contact_forms", force: :cascade do |t|
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "records", force: true do |t|
-    t.string   "title"
-    t.string   "metadata"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "record_attachments", force: :cascade do |t|
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "record_id"
+    t.text     "annotation"
     t.string   "file_upload_file_name"
     t.string   "file_upload_content_type"
     t.integer  "file_upload_file_size"
     t.datetime "file_upload_updated_at"
+    t.text     "file_upload_url"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string   "title"
+    t.string   "metadata"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "cas_user_name"
     t.boolean  "include_name"
     t.string   "content_type"
@@ -43,7 +51,7 @@ ActiveRecord::Schema.define(version: 20160419171046) do
     t.text     "hashtag"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "cas_user_name"
     t.boolean  "is_admin"
     t.datetime "created_at"
