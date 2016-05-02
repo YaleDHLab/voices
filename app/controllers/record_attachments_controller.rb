aa6bdf3f10bb9e791172408badfe6243b3ca62f4
duhaime <douglas.duhaime@gmail.com>
2016-05-02 10:57:12 -0400

    add methods to record_attachments controller to support raw object creation

--- app/controllers/record_attachments_controller.rb
+++ app/controllers/record_attachments_controller.rb
@@ -2,7 +2,19 @@ class RecordAttachmentsController < ApplicationController
 
   before_filter CASClient::Frameworks::Rails::Filter
 
-  # PATCH/PUT /record_attachments/1.json
+  def new
+    @record_attachment = RecordAttachment.new
+  end
+
+  def create
+    # add the cas username to the hash we pass to the db
+    @record_attachment_params = record_attachment_params()
+    @record_attachment_params[:cas_user_name] = session[:cas_user]
+    @record_attachment = RecordAttachment.create( @record_attachment_params )
+    render :nothing => true
+  end
+
+  # PATCH/PUT /attachments/1.json
   def update_annotation
     @record_attachment = RecordAttachment.find(params[:id])
     @record_attachment.update_attributes(annotation: params[:annotation])
@@ -13,5 +25,8 @@ class RecordAttachmentsController < ApplicationController
   end
 
   private
+    def record_attachment_params
+      params.require(:record_attachment).permit(:file_upload, :cas_user_name, :annotation)
+    end
 
 end
