4ed1f54d9c888a94ba43888a2cd9eb051185b925
duhaime <douglas.duhaime@gmail.com>
2016-05-06 10:44:46 -0400

    update voices app to include pagination and variagated view states

--- app/assets/javascripts/voices_app.js
+++ app/assets/javascripts/voices_app.js
@@ -63,7 +63,7 @@ VoicesApp.controller("FormController", [
 
     // setter for form elements; only to be called when user is editing record
     // TODO: Make into a service that returns response.data.record
-    $scope.fetchAttachments = function(recordId) {
+    $scope.getAttachments = function(recordId) {
       if (recordId) {
           $http.get("/records/" + recordId + ".json")
         .then(function(response) {
@@ -97,7 +97,7 @@ VoicesApp.controller("FormController", [
 
     // save the recordId for future requests
     $scope.recordId = recordId;
-    $scope.fetchAttachments($scope.recordId);
+    $scope.getAttachments($scope.recordId);
 
     // nb: to submit a record form with current params, we can call:
     //postRecordForm.uploadForm($scope.form);
@@ -177,8 +177,6 @@ VoicesApp.controller("FormController", [
 
 
 
-
-
 // provide a filter we can use to trust resources requested from aws
 VoicesApp.filter('trusted', [
       "$sce", 
@@ -189,6 +187,7 @@ VoicesApp.filter('trusted', [
 }]);
 
 
+
 VoicesApp.controller("GalleryController", [
         "$scope", "$http", "$location",
   function($scope, $http, $location) {
@@ -197,10 +196,14 @@ VoicesApp.controller("GalleryController", [
     // always start user on page 0
     $scope.currentPage = 0
 
-    // define an empty array of elements to hide, add to this list if user deletes an attachment
+    // global value that determines how many attachments a record needs before we
+    // break into the superuser pagination
+    $scope.superUserBreakpoint = 20;
+
+    // array of elements to hide (in case user deletes attachment)
     $scope.hiddenAttachments = [];
     
-    $scope.fetchRecordId = function() {
+    $scope.getRecordId = function() {
       
       // on page load, parse the requested record and page number from the params
       var url = window.location.href;
@@ -210,30 +213,82 @@ VoicesApp.controller("GalleryController", [
       if (url.indexOf("/edit") > -1) {
         url = url.replace("/edit", "");
       }
-
       // then parse the record id
       if (url.indexOf("/records/") > -1) {
         $scope.recordId = Number( url.split("records/")[1].split("?")[0].split(".")[0] );
       } 
-
       if (url.indexOf("/annotate/") > -1) {
         $scope.recordId = Number( url.split("annotate/")[1].split("?")[0].split(".")[0] );
       }
-
-      // once the recordId is available, initialize the page
-      $scope.fetchAttachments($scope.recordId, $scope.currentPage);
-
+    
+      // once the recordId is available, fetch json detailing the record and its attachments
+      $scope.getAttachments($scope.recordId, $scope.currentPage);
     };
 
-    // if user interacts with navigation, serve new attachments
-    $scope.fetchAttachments = function(recordId, pageNumber) {
+
+    // function to expose the current site to the client
+    $scope.getCurrentPageClass = function() {
+      var url = window.location.href;
+      if (url.indexOf("/edit") > -1) {
+        $scope.currentPageClass = "edit";
+      }
+      if (url.indexOf("/annotate") > -1) {
+        $scope.currentPageClass = "annotate";
+      }
+    };
+
+
+    $scope.setAttachmentsPerPage = function() {
+      var url = window.location.href;
+
+      // the gallery view should hold 20 images per page for all views except annotate, iff
+      // there are more than n attachments for the record 
+      if (url.indexOf("/annotate") > -1) {
+        $scope.attachmentsPerPage = 4;
+      }
+
+      if ($scope.data.attachments.length > $scope.superUserBreakpoint) {
+        $scope.attachmentsPerPage = $scope.attachmentsPerPage ? $scope.attachmentsPerPage : 20;
+      }
+
+      // use ternary operator to finalize attachments per page
+      $scope.attachmentsPerPage = $scope.attachmentsPerPage ? $scope.attachmentsPerPage : 4;
+      
+      // store the initial attachmentsPerPage for restoring attachmentsPerPage on view toggle
+      $scope.initialAttachmentsPerPage = $scope.attachmentsPerPage;
+
+      console.log("maximum attachments per page is", $scope.attachmentsPerPage);
+    };
+
+
+    // set the number of pages for the current record
+    $scope.setTotalNumberOfPages = function() {
+      // subtract one to account for 0 based indexing
+      $scope.totalNumberOfPages = Math.ceil( $scope.data.attachments.length / $scope.attachmentsPerPage );
+    };
+
+
+    // returns json with 'record' and 'attachments' keys
+    // the value of the attachment key is an array of attachments,
+    // each of which is a hashtable
+    $scope.getAttachments = function(recordId) {
+      console.log("getting attachments");
+
       if (recordId) {
-          $http.get("/records/" + recordId + ".json" + "?page=" + pageNumber)
+          $http.get("/records/" + recordId + ".json")
         .then(function(response) {
           // on success
           $scope.data = response.data;
-          $scope.currentPage = pageNumber;
-          
+
+          // use the data to set the attachments per page
+          $scope.setAttachmentsPerPage();
+
+          // fetch the first page of results
+          $scope.getPageOfAttachments(0);
+
+          // call function to make total number of pages available to functions
+          $scope.setTotalNumberOfPages()
+
         }, // on failure
           function(response) {
             console.log("http request failed", response.status);
@@ -242,25 +297,65 @@ VoicesApp.controller("GalleryController", [
       } 
     };
 
-    // build up an array of page numbers
-    $scope.pageArray = function(numberOfPages) {
+
+    // function to determine which image url to serve to the client
+    $scope.getImageUrl = function(attachment) {
+      if ($scope.attachmentsPerPage == 1) {
+        return attachment.medium_image_url;
+      }
+      if ($scope.attachmentsPerPage == 4) {
+        return attachment.annotation_thumb_url;
+      }
+      if ($scope.attachmentsPerPage == 20) {
+        return attachment.square_thumb_url;
+      }
+    };
+
+
+    // function that returns all attachments on a aparticular page 
+    // of a record, and updates the current page in scope to the retrieved
+    // page
+    $scope.getPageOfAttachments = function(pageNumber) {
+      console.log("requested page", pageNumber);
+
+      // from the full array of attachments, select the range between
+      // attachmentsPerPage * currentPage and attachmentsPerPage * (currentPage + 1)
+      var startingAttachmentIndex = $scope.attachmentsPerPage * pageNumber;
+      var endingAttachmentIndex = $scope.attachmentsPerPage * (pageNumber + 1);
+
+      $scope.attachmentsOnCurrentPage = $scope.data.attachments.slice(
+        startingAttachmentIndex, endingAttachmentIndex
+      ); 
+
+      // update the current page in the scope
+      $scope.currentPage = pageNumber;
+
+      console.log("current attachments:", $scope.attachmentsOnCurrentPage, "current page:", $scope.currentPage);
+    };
+
+
+    // build up an array of page numbers [= xrange(nPages)]
+    $scope.getPageArray = function(numberOfPages) {
       return new Array(numberOfPages);
     };
+
 
     // functions to send users to the {previous, next} page
     $scope.previousPage = function() {
       if ($scope.currentPage > 0) {
         $scope.currentPage = $scope.currentPage - 1;
       }
-      $scope.fetchAttachments($scope.recordId, $scope.currentPage);
+      $scope.getPageOfAttachments($scope.currentPage);
     }
 
+
     $scope.nextPage = function() {
-      if ($scope.currentPage < $scope.data.number_of_pages) {
+      if ($scope.currentPage < $scope.totalNumberOfPages) {
         $scope.currentPage = $scope.currentPage + 1;
       }
-      $scope.fetchAttachments($scope.recordId, $scope.currentPage);
+      $scope.getPageOfAttachments($scope.currentPage);
     }
+
 
     // save user annotations (send CSRF token for security)
     $scope.saveAnnotation = function(userAnnotation, attachmentId) {      
@@ -271,6 +366,7 @@ VoicesApp.controller("GalleryController", [
         success: function(response) {}
       });
     };
+
 
     // on record#edit, allow users to delete an attachment on click of button
     $scope.deleteAttachment = function(attachmentId) {
@@ -283,16 +379,53 @@ VoicesApp.controller("GalleryController", [
         data: {},
         success: function(response) {}
       });
-
     }
 
+
+    $scope.multipleRecordViewClicked = function() {
+      // toggle the view buttons
+      $scope.multipleRecordView = true;
+
+      // determine the number of attachments per page
+      $scope.attachmentsPerPage = $scope.initialAttachmentsPerPage;
+
+      // reset the total number of pages
+      $scope.setTotalNumberOfPages();
+
+      // restore the 0th page of attachments
+      $scope.getPageOfAttachments(0);
+
+      console.log("multiple records clicked", $scope.attachmentsPerPage);
+    };
+
+
+    $scope.singleRecordViewClicked = function() {
+      $scope.multipleRecordView = false;
+      
+      // set things so that we can only have one record per page
+      // and start us off on the 0th page
+      $scope.attachmentsPerPage = 1;
+
+      // reset the total number of pages
+      $scope.setTotalNumberOfPages();
+
+      $scope.getPageOfAttachments(0);
+
+      console.log("single record clicked", $scope.attachmentsPerPage);
+    };
+
     // initialize the function chain
-    $scope.fetchRecordId();
-  
+    $scope.getRecordId();
+
+    // call an additional function to make current page available to client
+    $scope.getCurrentPageClass();
+
+    // initialize the view with multi-record view
+    $scope.multipleRecordView = true;
+
+
   }
 ]);
-
-
 
 
 
@@ -339,8 +472,6 @@ VoicesApp.controller("recordSearchController", [
     }
   ]
 );
-
-
 
 
 
