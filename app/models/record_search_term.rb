360bc67ec87a6213a14a607f439ec7b049b3ae37
duhaime <douglas.duhaime@gmail.com>
2016-05-07 09:07:52 -0400

    allow users to search by records that start or end with the input string

--- app/models/record_search_term.rb
+++ app/models/record_search_term.rb
@@ -9,16 +9,16 @@ class RecordSearchTerm
 
   def build_field_search(search_term)
     @where_clause << case_insensitive_search(:description)
-    @where_args[:description] = starts_with(search_term)
+    @where_args[:description] = starts_or_ends_with(search_term)
 
     @where_clause << " OR #{case_insensitive_search(:title)}"
-    @where_args[:title] = starts_with(search_term)
+    @where_args[:title] = starts_or_ends_with(search_term)
 
     @order = "title asc"
   end
 
-  def starts_with(search_term)
-    search_term + "%"
+  def starts_or_ends_with(search_term)
+    "%" + search_term + "%"
   end
 
   def case_insensitive_search(field_name)
