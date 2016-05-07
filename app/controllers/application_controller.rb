13b5cc58d3bd83b1f42b6e1423a86d9a7783b424
duhaime <douglas.duhaime@gmail.com>
2016-05-07 11:59:35 -0400

    update application controller to create 3 tiers of access: allow admins to call records#index, allow creators to call records#edit, update, destroy, and allow pucas public to access records they created and public records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  ###
+  # Create three levels of access:
+  #   users can access records they created and records others have made public
+  #   only creators of objects can edit, update, and destroy those objects
+  #   only admins can access the index method in records controller
+  ###
+
+  # method that grants each cas user access to records they created and records
+  # others have made public
   def check_user_privileges(requested_record)
     # if the requested record does not belong to the current user
     # and is private, prevent the user from accessing the record
@@ -12,6 +21,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # method that grants only the creator of a record edit, update, and delete privileges
+  def check_private_user_privileges(requested_record)
+    # if the requested record does not belong to the current user
+    # and is private, prevent the user from accessing the record
+    if (requested_record.cas_user_name != session[:cas_user])
+      flash[:info] = "<strong>ACCESS RESTRICTED</strong>".html_safe + ": You do not have access to this page. Please contact your administrator about your permissions."
+      redirect_to user_show_path
+    end
+  end
+
+  # method that grants only admins access to records#index method
   def check_admin_privileges()
     site_admins = ENV["VOICES_ADMINS"].split("#")
     if not site_admins.include? session[:cas_user]
