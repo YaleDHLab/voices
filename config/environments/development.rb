24dcd707a5cc285a97f9d3146913e0ec5325061c
duhaime <douglas.duhaime@gmail.com>
2016-05-11 16:04:42 -0400

    configure paperclip to use https

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,7 @@ VoicesRails::Application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => :https,
     :s3_credentials => {
       :bucket => ENV['AWS_S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
