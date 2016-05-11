a5596b5904afe530f18a3aff7cf29f9dd20baf0f
duhaime <douglas.duhaime@gmail.com>
2016-05-11 13:19:30 -0400

    update method used to fetch glyphicon fonts to prevent 404's

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,12 @@ module VoicesRails
     # Add bootstrap fonts to the asset path (see also static_pages.css.scss, which overrides bootstrap's
     # default path for requested glyphicon assets in order to point to the asset_path())
     config.assets.enabled = true
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
+    # recursively add font elements to asset path
+    Dir.glob("#{Rails.root}/vendor/assets/fonts/**").each do |path|
+      config.assets.paths << path
+    end
+
 
     # One can now to reference glyphicons/glyphicons-halflings-regular.woff; see
     # static_pages.css.scss for the implementation there
