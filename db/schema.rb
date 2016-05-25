3daad3a3d2be16890a6d8c21df2128b48e53642f
duhaime <douglas.duhaime@gmail.com>
2016-05-25 14:53:03 -0400

    add client side timestamp to record attachment attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523231801) do
+ActiveRecord::Schema.define(version: 20160524214511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160523231801) do
     t.text     "medium_image_url"
     t.text     "annotation_thumb_url"
     t.text     "square_thumb_url"
+    t.string   "client_side_timestamp"
   end
 
   create_table "records", force: :cascade do |t|
