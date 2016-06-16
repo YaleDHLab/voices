1d061dcc0c94e957d6cf3708e32e085666c1baa0
duhaime <douglas.duhaime@gmail.com>
2016-06-16 17:08:52 -0400

    ensure seed file loads images into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-#This file should contain all the record creation needed to seed the database with its default values.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
@@ -6,56 +6,51 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-tmp = File.read(Rails.root.join('lib/assets/DOWN.txt')).scan(/.{2500}/)
 
+# seed each admin's database if admins are defined, else seed
+# random usernames and warn at the console
+stored_admin_usernames = ENV["VOICES_ADMINS"]
 
-#img = File.new( Dir.glob("#{Rails.root}/app/assets/images/seed-images/*.jpg")[rand(0..249)] )
-txt = File.new( Dir.glob("#{Rails.root}/app/assets/texts/sample.txt")[0])
+if stored_admin_usernames.nil?
+    user_name_array = ["sample_user_1", "sample_user_2", "sample_user_3"]
+else
+    user_name_array = stored_admin_usernames.split("#")
+end
 
+user_name_array.each do |username|
 
-
-# seed each admin's database
-ENV["VOICES_ADMINS"].split("#").each do |username|
-	
-'''
-    if username == "ded34"
-        next
-    end
-    '''
-
-    tmp.each do |desc|
+    7.times do |r|
 
         new_record = Record.new({
             :title => Faker::Book.title,
             :cas_user_name => username,
             :make_private => false,
-            :description => desc,
-	    :location => Faker::Address.street_name + ", " + Faker::Address.city,
+            :description => Faker::Lorem.paragraph,
+            :location => Faker::Address.street_name + ", " + Faker::Address.city,
             :source_url => Faker::Internet.url,
             :release_checked => true,
-            :date => DateTime.now-rand(0.1..1),
+            :date => Faker::Time.between(DateTime.now - 1, DateTime.now).to_s.split()[0].gsub(/-/, '/'),
             :hashtag => "#" + Faker::Hipster.words(4).join(" #")
         })
 
         i = new_record.save!
 
         # create eather 1, 8, or 24 attachments for this record
-        #[1, 8, 24][rand(0..2)].times do
+        [1, 8, 24][rand(0..2)].times do
             new_record_attachment = RecordAttachment.new({
                 :record_id => new_record.id,
                 :annotation => Faker::Lorem.sentence,
-                :file_upload => txt, 
+                :file_upload => File.new( Dir.glob("#{Rails.root}/app/assets/images/seed-images/*.jpg")[rand(0..249)] ),
                 :cas_user_name => username,
                 :is_seed => true
             })
 
             new_record_attachment.save!
-        #end 
+        end 
     end
 end
 
-
-#initiate database for Cloud on homepage.
+# create new cloudword model
 CloudWord.delete_all
 
 cloud = CloudWord.new()
